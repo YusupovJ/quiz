22365c8c809bd54f9b88ec2f2968c61577f940ba
YusupovJ <jamshudanama@gmail.com>
2026-05-20 21:31:35 +0500

    fix total ticket

--- src/pages/Cyber/CyberQuiz .tsx
+++ src/pages/Cyber/CyberQuiz .tsx
@@ -4,7 +4,7 @@ import { cyber } from "@/data/cyber";
 
 export const CyberQuiz = () => {
   const { index, id } = useParams();
-  const questions = cyber[Number(index)];
+  const questions = index === "total" ? cyber.flat() : cyber[Number(index)];
   const question = questions.find((question) => question.id === Number(id))!;
   const isLast = question.id === questions[questions.length - 1].id;
 
