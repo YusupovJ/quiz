91758a6b850e9de90cdaf2fbc883c631c3d9453b
YusupovJ <jamshudanama@gmail.com>
2026-05-27 17:13:32 +0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,15 @@ const root = createRoot(rootElement);
 root.render(
   <BrowserRouter>
     <App />
+    <iframe
+      id="12116"
+      className="wctv-video"
+      title="Video 1 - The Importance Of Multi time Frame Trading To Professional Traders"
+      src="https://player.vimeo.com/video/362785793?h=9e47e476e0"
+      frameborder="0"
+      allow="autoplay"
+      allowfullscreen
+    />
     <Toaster />
-  </BrowserRouter>
+  </BrowserRouter>,
 );
